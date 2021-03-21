99f6ad3626fd0de942d333c9c4f1f5d5621cc52f
MacGregor <57849043+macObl@users.noreply.github.com>
2021-03-21 17:54:48 -0400

    Add files via upload

---
+++ Course Registration/index.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="App_Themes/SiteStyles.css" />
+    <style type="text/css">
+        #Button1 {
+        }
+        #form1 {
+            height: 327px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Algonquin College Course Registration</h1>
+    <asp:Panel runat="server" ID="Regestration"> 
+        <form id="form1" runat="server">
+            <div style="float:left">
+            Student Name:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtName" runat="server" Width="183px" CssClass="input"></asp:TextBox>
+            </div>
+            <div style="float:left">
+                <asp:RadioButtonList ID="studentType" runat="server" RepeatDirection="Horizontal">
+                    <asp:ListItem Value="full-time" Selected="True">Full Time</asp:ListItem> 
+                    <asp:ListItem Value="part-time">Part Time</asp:ListItem>
+                    <asp:ListItem Value="co-op">Co-op</asp:ListItem>
+                </asp:RadioButtonList>
+            </div>
+    &nbsp;
+        
+            <p>Follwing courses are currently available for registration</p>
+            
+            <asp:Label ID="error" runat="server" CssClass="emphsis"></asp:Label>
+            <asp:CheckBoxList ID="liCourses" runat="server">
+            </asp:CheckBoxList>
+        
+            
+           <asp:Button runat="server" ID="submit" OnClick="Submit_Click" Height="32px" Text="Submit" Width="80px" CssClass="button" />
+            
+
+        </form>
+    </asp:Panel>
+
+    <asp:Panel runat="server" ID="Results">
+
+        <p>Thank you <asp:Label ID="Name" runat="server" CssClass="emphsis"></asp:Label>, for using our online regisration system</p>
+        <p>You have been registered as a <asp:Label ID="Type" runat="server" CssClass="distinct"></asp:Label> for the following courses</p>
+        <br />
+        <asp:Table ID="Table" runat="server" CssClass="table">
+            <asp:TableRow>
+                <asp:TableHeaderCell>Course Code</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Course Title</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Weekly Hours</asp:TableHeaderCell>
+                <asp:TableHeaderCell>Fee Payable</asp:TableHeaderCell>
+            </asp:TableRow>
+        </asp:Table>
+
+    </asp:Panel>
+    </body>
+
+</html>
---
+++ Course Registration/index.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        List<Course> course = Helper.GetCourses();
+        for (int i = 0; i < course.Count; i++)
+        {
+            Course courses = course[i] as Course;
+            ListItem item = new ListItem(course[i].ToString(), i.ToString());
+            liCourses.Items.Add(item);
+        }
+        Regestration.Visible = true;
+        Results.Visible = false;
+    }
+
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            List<Course> course = Helper.GetCourses();
+            Student student = null;
+
+            //Choses the type of student
+            switch (studentType.SelectedItem.Value)
+            {
+                case "full-time":
+                    student = new FullTimeStudent(txtName.Text);
+                    break;
+                case "part-time":
+                    student = new PartTimeStudent(txtName.Text);
+                    break;
+                case "co-op":
+                    student = new PartTimeStudent(txtName.Text);
+                    break;
+            }
+            //Add the couses to the student 
+            foreach(ListItem item in liCourses.Items)
+            {
+                if (item.Selected)
+                {
+                    int selected = int.Parse(item.Value);
+                    student.addCourse(course[selected] as Course);
+                    
+                }
+            }
+
+            //Throw Errors
+            if (txtName.Text == "")
+            {
+                throw new Exception("You need to type your name in the space provided");
+            }
+            if (student.getEnrolledCourses().Count == 0)
+            {
+                throw new Exception("You need to select a cource");
+            }
+
+            //Makes table from selected courses
+            foreach (Course courses in student.getEnrolledCourses())
+            {
+                TableRow row = new TableRow();
+                TableCell cell = new TableCell();
+
+                cell.Text = courses.Code;
+                row.Cells.Add(cell);
+
+                cell = new TableCell();
+                cell.Text = courses.Title;
+                row.Cells.Add(cell);
+
+                cell = new TableCell();
+                cell.Text = courses.WeeklyHours.ToString();
+                row.Cells.Add(cell);
+
+                cell = new TableCell();
+                cell.Text ="$" + courses.Fee.ToString();
+                row.Cells.Add(cell);
+
+                Table.Rows.Add(row);
+            }
+            //Makes the Total of the table
+            TableRow rowTotal = new TableRow();
+            TableCell cellTotal = new TableCell();
+
+            cellTotal.Text = "Total:";
+            cellTotal.ColumnSpan = 2;
+            rowTotal.Cells.Add(cellTotal);
+
+            cellTotal = new TableCell();
+            cellTotal.Text = student.totalWeeklyHours().ToString();
+            rowTotal.Cells.Add(cellTotal);
+
+            cellTotal = new TableCell();
+            cellTotal.Text = "$" + student.feePayable().ToString();
+            rowTotal.Cells.Add(cellTotal);
+            
+            Table.Rows.Add(rowTotal);
+
+            Name.Text = txtName.Text;
+            Type.Text = studentType.SelectedItem.Text;
+
+            Regestration.Visible = false;
+            Results.Visible = true;
+            
+        }
+        catch (Exception x)
+        {
+            error.Text = x.Message;
+        }
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+}
